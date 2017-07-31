15b02d4bbe5d584a25a66facfa26d87e2e3faf51
mcashman2 <mikaela.cashman@gmail.com>
2017-07-31 11:14:32 -0400

    fbafiles pushed

---
+++ fbafiles/GA/runMFAToolkitGA.sh
@@ -0,0 +1,1 @@
+/kb/deployment/bin/mfatoolkit resetparameter "MFA input directory" "/kb/module/work/fbafiles/GA/ReactionDB/" parameterfile "/kb/module/work/fbafiles/GA/SpecializedParameters.txt" LoadCentralSystem "/kb/module/work/fbafiles/GA/Model.tbl" > "/kb/module/work/fbafiles/GA/log.txt"
